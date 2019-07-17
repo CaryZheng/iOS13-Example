3049e3d4806845d84303bf39d25dca240df959a5
Cary <CaryZhengMy@gmail.com>
2019-07-17 11:53:20 +0800

    Initial Commit

---
+++ iOS13Example/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOS13Example
+//
+//  Created by Cary on 2019/7/17.
+//  Copyright © 2019 Cary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
